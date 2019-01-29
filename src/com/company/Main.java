3ef01fd24fcebba8eeaa503e6856208911e4c957
eyael <eyaelja1@gmail.com>
2019-01-29 10:45:41 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner Keyboard = new Scanner(System.in);
+        System.out.println("enter number");
+        int num=Keyboard.nextInt();
+
+        int num1= num + 5;
+        int num2= num1 * 2;
+        int num3= num2 - 7;
+        System.out.println(num3);
+
+
+
+
+
+
+
+	// write your code here
+    }
+}
